b283b95473f8d715989f173b628025c782d7a2ab
Diego Carmona <diegocarmona@my.whccd.edu>
2024-01-19 21:10:48 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,45 @@
+﻿/* 
+ * Author: Diego Carmona
+ * Course: Intro prog concepts Method C#
+ * Purpose: To Gain familiarity with C# and visual studio application. 
+*/
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("30 things bout me");
+            Console.WriteLine("1. Name: Diego Carmona");
+            Console.WriteLine("2. Major: Full-Stack Web Development COA");
+            Console.WriteLine("3. Pets: 3 kinds");
+            Console.WriteLine("4. Pet classification: Dogs, Fish, Chickens");
+            Console.WriteLine("5. Fav Music Genre: Metal, Hard rock, rock");
+            Console.WriteLine("6. ");
+            Console.WriteLine("7. ");
+            Console.WriteLine("8. ");
+            Console.WriteLine("9. ");
+            Console.WriteLine("10. ");
+            Console.WriteLine("11. ");
+            Console.WriteLine("12. ");
+            Console.WriteLine("13. ");
+            Console.WriteLine("14. ");
+            Console.WriteLine("15. ");
+            Console.WriteLine("16. ");
+            Console.WriteLine("17. ");
+            Console.WriteLine("18. ");
+            Console.WriteLine("19. ");
+            Console.WriteLine("20. ");
+            Console.WriteLine("21. ");
+            Console.WriteLine("22. ");
+            Console.WriteLine("23. ");
+            Console.WriteLine("24. ");
+            Console.WriteLine("25. ");
+            Console.WriteLine("26. ");
+            Console.WriteLine("27. ");
+            Console.WriteLine("28. ");
+            Console.WriteLine("29. ");
+            Console.WriteLine("30. ");
+        }
+    }
+}
